297a2ce6aed21814adc4b7235086f259f08b71c6
joseDanielRestrepoOrozco <jose.resoro4789@gmail.com>
2023-10-24 01:04:09 -0500

    creacion de ms de seguridad de urbaNav

---
+++ src/main/java/com/msurbaNavSecurity/msurbaNavSecurity/MsUrbaNavSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.msurbaNavSecurity.msurbaNavSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUrbaNavSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUrbaNavSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msurbaNavSecurity/msurbaNavSecurity/MsUrbaNavSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msurbaNavSecurity.msurbaNavSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUrbaNavSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
