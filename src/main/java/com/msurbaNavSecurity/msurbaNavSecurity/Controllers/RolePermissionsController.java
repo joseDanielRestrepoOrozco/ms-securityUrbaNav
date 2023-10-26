64668df291bdc7f16b39f53d4f959ce9d90e64a8
MiguelCardona11 <04.migue.cardona@gmail.commit>
2023-10-26 13:17:05 -0500

    implementacion clase RolePermission

---
+++ src/main/java/com/msurbaNavSecurity/msurbaNavSecurity/Controllers/RolePermissionsController.java
@@ -0,0 +1,67 @@
+package com.msurbaNavSecurity.msurbaNavSecurity.Controllers;
+
+import com.msurbaNavSecurity.msurbaNavSecurity.Models.Permission;
+import com.msurbaNavSecurity.msurbaNavSecurity.Models.Role;
+import com.msurbaNavSecurity.msurbaNavSecurity.Models.RolePermission;
+import com.msurbaNavSecurity.msurbaNavSecurity.Repositories.PermissionRepository;
+import com.msurbaNavSecurity.msurbaNavSecurity.Repositories.RolePermissionRepository;
+import com.msurbaNavSecurity.msurbaNavSecurity.Repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+// @RequestMapping("api/role-permission")
+@RequestMapping("/role-permission")
+
+public class RolePermissionsController {
+    @Autowired
+    private RolePermissionRepository theRolePermissionRepository;
+
+    @Autowired
+    private RoleRepository theRoleRepository;
+
+    @Autowired
+    private PermissionRepository thePermissionRepository;
+
+    // Método POST
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("role/{role_id}/permission/{permission_id}")
+    public RolePermission store(@PathVariable String role_id,
+                                @PathVariable String permission_id) {
+        Role theRole=theRoleRepository.findById(role_id).orElse(null);
+        Permission thePermission=thePermissionRepository.findById(permission_id).orElse(null);
+        if(theRole!=null && thePermission!=null){
+            RolePermission newRolePermission = new RolePermission();
+            newRolePermission.setRole(theRole);
+            newRolePermission.setPermission(thePermission);
+            return this.theRolePermissionRepository.save(newRolePermission);
+        } else {
+            return null;
+        }
+    }
+
+    // Método GET (TODOS)
+    @GetMapping("")
+    public List<RolePermission> index(){
+        return this.theRolePermissionRepository.findAll();
+    }
+
+    // Método DELETE
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void destroy(@PathVariable String id) {
+        RolePermission theRoleRolePermission = this.theRolePermissionRepository
+                .findById(id)
+                .orElse(null);
+        if (theRoleRolePermission != null) {
+            this.theRolePermissionRepository.delete(theRoleRolePermission);
+        }
+    }
+
+
+}
+
---
+++ src/main/java/com/msurbaNavSecurity/msurbaNavSecurity/Repositories/RolePermissionRepository.java
@@ -0,0 +1,13 @@
+package com.msurbaNavSecurity.msurbaNavSecurity.Repositories;
+
+import com.msurbaNavSecurity.msurbaNavSecurity.Models.RolePermission;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface RolePermissionRepository extends MongoRepository<RolePermission, String> {
+
+    // método para encontrar RolePermission con rol y permiso (consultando a la BD)
+    @Query("{'role.$id': ObjectId(?0),'permission.$id': ObjectId(?1)}")
+    RolePermission getRolePermission(String roleId,String permissionId);
+}
+
